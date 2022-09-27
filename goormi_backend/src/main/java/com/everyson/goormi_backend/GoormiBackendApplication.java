06c7e89cabf478f4e69561c5d67c1e5996da8aa6
muyongKim <wykim970@gmail.com>
2022-09-28 01:10:57 +0900

    first commit: backend

---
+++ goormi_backend/src/main/java/com/everyson/goormi_backend/GoormiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.everyson.goormi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormiBackendApplication.class, args);
+    }
+
+}
---
+++ goormi_backend/src/test/java/com/everyson/goormi_backend/GoormiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyson.goormi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
